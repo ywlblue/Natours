b178873eec797dcf374a2bae9a6cc28a7bb9fe3f
Weilan Yu <isweilanyu@gmail.com>
2021-01-21 22:59:12 -0800

    Database error handling

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.values}`;
+    return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+    const message = `Duplicate field value: ${value}. Please use another value!`
+    return new AppError(message, 400);
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -36,7 +56,12 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        // transform CastError
+        let error = {...err};
+        if(error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        sendErrorProd(error, res);
     }
 
     
--- server.js
+++ server.js
@@ -18,4 +18,22 @@ mongoose.connect(DB, {
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
+});
+
+// Handle unhandled rejection
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! Shutting down!')
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 })
+
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down!')
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+    console.log()
+});
